0398fab90ab7052de7a9379f3c57a784b19393a5
Shaun Underwood <su@sunderwood.co.uk>
2021-04-22 08:16:29 +0200

    Added TextInput to widgets

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,9 +1,11 @@
 import DeleteButton from './DeleteButton'
 import EditableLabel from './EditableLabel'
 import InlineInput from './InlineInput'
+import TextInput from './TextInput'
 
 export default {
   DeleteButton,
   EditableLabel,
-  InlineInput
+  InlineInput,
+  TextInput
 }
